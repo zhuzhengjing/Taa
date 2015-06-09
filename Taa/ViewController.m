6b1fc4d53608278e10dc37de040d0bfb756448fa
rainlesvio <rainlesvio@163.com>
2015-06-09 12:46:43 +0800

    Initial Commit

---
+++ Taa/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Taa
+//
+//  Created by wuxueying on 6/9/15.
+//  Copyright (c) 2015 wuxueying. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Taa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Taa
+//
+//  Created by wuxueying on 6/9/15.
+//  Copyright (c) 2015 wuxueying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
